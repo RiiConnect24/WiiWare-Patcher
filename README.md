ebb5df7deaa4d9ab441cbcde60c699a0072fbaf1
Larsenv <larsen@weborican.com>
2016-10-20 16:46:23 -0500

    New patcher added

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# auto-wiiware-patcher
-WiiWare patching for Wiimmfi made easy.
+# Automatic WiiWare Patcher
+
+This patcher will make WiiWare patching easy to use Wiimmfi. It also supports patching the Wii Speak Channel and the Mario Kart Channel.
+
+## Usage
+
+Put your WADs (there can be multiple of them) in the folder corresponding to your operating system then run the patch script.
+
+## Credits
+- Person66 for Sharpii.
+
+## Changelog
+
+v1.1:
+- Rewrote the script.
+- Allowed multiple WADs to be patched at once.
+
+v1.0:
+- Initial Release
---
+++ Windows/patch.bat
@@ -0,0 +1,25 @@
+echo off
+echo Automatic WAD Patcher for Wiimmfi.
+echo Patcher by PokeAcer and Larsenv. Sharpii by Person66.
+echo This will patch any WiiWare games in this folder to be installed using a WAD Manager.
+
+echo.
+
+mkdir wiimmfi-wads
+
+for %%f in ("*.wad") do (
+	echo Patching %%~nf...
+	Sharpii.exe WAD -u "%%f" temp
+	move temp\00000001.app 00000001.app
+	WiiWarePatcher.exe
+	move 00000001.app temp\00000001.app
+	del "%%f"
+	Sharpii.exe WAD -p temp "wiimmfi-wads/%%f"
+	rmdir temp /s /q
+)
+
+echo.
+
+echo Done patching the WADs. They're in the folder named wiimmfi-wads.
+
+pause
